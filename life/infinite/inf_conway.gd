163c9b102943edd086d7de349b8e27d9c70c2fde
TeoRam23 <teoram23@vgs.nfk.no>
2024-05-14 14:29:04 +0200

    begynte på game of life Infinite
    game of life men det er ikke på en eksisterende grid.
    funker ikke enda.

---
+++ life/infinite/inf_conway.gd
@@ -0,0 +1,126 @@
+extends StaticBody2D
+
+var ALIVE = false
+var pausing = true
+
+var gonna_change = false
+
+var size = 0
+
+@onready var control = $Control
+@onready var sprite = $Sprite2D
+@onready var conway_detector = $ConwayDetector
+
+var CARC = preload("res://life/infinite/inf_conway.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_parent().inf_toggle_life.connect(tole_life)
+	get_parent().inf_update_please.connect(lets_update)
+	get_parent().inf_lets_carcass.connect(place_carcass)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	conway_detector.set_collision_mask_value(5, true)
+	if pausing:
+		set_population()
+	else:
+		do_the_conway()
+
+
+func do_the_conway():
+	
+	var neighbors = []
+	if conway_detector.get_overlapping_bodies():
+		neighbors = conway_detector.get_overlapping_bodies()
+	var frens = 0
+	for bro in neighbors:
+#		print("Bro pos: ", bro.position)
+		if bro.ALIVE and bro != self:
+			frens += 1
+#	print("Col: ", collision_layer)
+#	print(frens, ", ", position)
+
+	if ALIVE:
+		if frens < 2:
+			gonna_change = true
+		elif frens == 2 or frens == 3:
+			gonna_change = false
+		elif frens > 3:
+			gonna_change = true
+	elif !ALIVE and frens == 3:
+			gonna_change = true
+	else:
+		gonna_change = false
+
+
+func lets_update():
+	if gonna_change:
+		change_state()
+
+
+func change_state():
+	ALIVE = !ALIVE
+	if ALIVE:
+		modulate = Color(0.604, 0.871, 0.557, 0.999)
+		set_collision_layer_value(1, true)
+
+	if !ALIVE:
+		queue_free()
+
+
+func set_population():
+#	if Input.is_action_pressed("LMB") and !ALIVE:
+#		var mouse_position = get_global_mouse_position()
+#		if control.get_rect().has_point(to_local(mouse_position)):
+#			print("get changed!")
+#			change_state()
+
+	if Input.is_action_pressed("RMB") and ALIVE:
+		var mouse_position = get_global_mouse_position()
+		if control.get_rect().has_point(to_local(mouse_position)):
+			print("get changed!")
+#			change_state()
+			queue_free()
+
+
+func place_carcass():
+	if !ALIVE:
+		return
+	conway_detector.set_collision_mask_value(5, true)
+	
+#	print(position, ", ", size)
+	for y in 3:
+		for x in 3:
+#			print(x, ", ", y)
+			var new_x = position.x - size + (size * x)
+			var new_y = position.y - size + (size * y)
+			
+			if conway_detector.get_overlapping_bodies():
+				var bean = false
+				var neighbors = conway_detector.get_overlapping_bodies()
+				for bro in neighbors:
+					if bro.position == Vector2(new_x, new_y):
+						bean = true
+						break
+				if bean:
+					continue
+
+############################################ de bryr seg ikke om de ny-plasserte likene. kanskje de ikke finnes enda og de dukker opp samtidig?
+
+			var new_carcass = CARC.instantiate()
+			
+			new_carcass.position.x = position.x - size + (size * x)
+			new_carcass.position.y = position.y - size + (size * y)
+			new_carcass.scale.x = size
+			new_carcass.scale.y = size
+			new_carcass.size = size
+			
+			
+			get_parent().add_child(new_carcass)
+
+
+
+func tole_life():
+	pausing = !pausing
---
+++ life/infinite/inf_grid.gd
@@ -0,0 +1,92 @@
+extends Node2D
+
+@onready var life_time = $LifeTimeI
+
+var SQAR = preload("res://life/infinite/inf_conway.tscn")
+
+var pausing = true
+
+@export var width = 20
+@export var height = 12
+@export var size = 16
+@export var set_time = 0.5
+@export var alive = false
+@export var random = false
+
+
+signal inf_toggle_life()
+signal inf_update_please()
+signal inf_lets_carcass()
+
+
+func _ready():
+	if set_time <= 0.01:
+		set_time = 0.02
+	life_time.wait_time = set_time
+#	place_grid()
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("start"):
+		pausing = !pausing
+		inf_toggle_life.emit()
+		if life_time.time_left:
+			life_time.stop()
+		else:
+			life_time.start()
+			
+	if pausing:
+		if Input.is_action_just_pressed("LMB"):
+			var mouse_position = get_local_mouse_position()
+			var pos_x = int(mouse_position.x / size)
+			var pos_y = int(mouse_position.y / size)
+			print(pos_x, ", ", pos_y)
+			place_conway(pos_x, pos_y)
+			
+			
+			
+
+
+
+func place_conway(x, y):
+	var children = get_children()
+	for kid in children:
+#		print("COming through")
+		if !kid.is_in_group("grp_conways"):
+#			print("Lest split up gang")
+			continue
+		elif kid.position == Vector2(x * size, y * size):
+#			print("WOAH")
+			return
+	var new_square = SQAR.instantiate()
+	
+	new_square.position.x = x * size
+	new_square.position.y = y * size
+	new_square.scale.x = size
+	new_square.scale.y = size
+	new_square.size = size
+	
+	new_square.change_state()
+	
+	add_child(new_square)
+
+
+#func _draw():
+#	var lightness = 0.5
+#	for y in height:
+#		var points = [Vector2(0, y * size), Vector2(width * size, y * size)]
+#		var color = [Color(lightness, lightness, lightness), Color(lightness, lightness, lightness)]
+#
+#		draw_polyline_colors(points, color, 0.3)
+#	for x in width:
+#		var points = [Vector2(x * size, 0), Vector2(x * size, height * size)]
+#		var color = [Color(lightness, lightness, lightness), Color(lightness, lightness, lightness)]
+#
+#		draw_polyline_colors(points, color, 0.3)
+		
+
+
+func _on_life_time_i_timeout():
+	print("here we go folks")
+	inf_update_please.emit()
+	inf_lets_carcass.emit()
