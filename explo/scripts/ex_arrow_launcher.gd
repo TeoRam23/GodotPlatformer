ca05458852935bea7d3638ddcdda290e40319632
TeoRam23 <teoram23@vgs.nfk.no>
2026-05-17 15:16:25 +0200

    2-A + 2-B
    - La til banene 2-A og 2-B
    - La til mulighet for arrow launcher til å ikke launche på starten
    - Fikset arrow launchers "fwoop" partikler, så de alltid vises når ned launcher arrow

--- explo/scripts/ex_arrow_launcher.gd
+++ explo/scripts/ex_arrow_launcher.gd
@@ -1,15 +1,23 @@
 extends StaticBody2D
 
 @onready var launch_timer = $LaunchTimer
-@onready var shoop_particle = $ShoopParticle
+@onready var shoop_particle_1: CPUParticles2D = $Particles/ShoopParticle1
+@onready var shoop_particle_2: CPUParticles2D = $Particles/ShoopParticle2
+@onready var shoop_particle_3: CPUParticles2D = $Particles/ShoopParticle3
+@onready var shoop_particle_4: CPUParticles2D = $Particles/ShoopParticle4
 
 const ARROW_HAZARD = preload("res://explo/hazards/arrow_hazard.tscn")
 @export var arrow_speed = -180.0
 @export var launch_seconds = 1.0
 
+@export var launch_on_start = true
+
+var picked_particle = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	launch_arrow()
+	if launch_on_start:
+		launch_arrow()
 	launch_timer.wait_time = launch_seconds
 	launch_timer.start()
 	#print("YEP ", launch_seconds)
@@ -28,6 +36,18 @@ func launch_arrow():
 	var new_arrow = ARROW_HAZARD.instantiate()
 	new_arrow.velocity = Vector2.RIGHT.rotated(rotation) * arrow_speed
 	
-	shoop_particle.emitting = true
+	if picked_particle == 1:
+		shoop_particle_1.emitting = true
+		picked_particle += 1
+	elif picked_particle == 2:
+		shoop_particle_2.emitting = true
+		picked_particle += 1
+	elif picked_particle == 3:
+		shoop_particle_3.emitting = true
+		picked_particle += 1
+	elif picked_particle == 4:
+		shoop_particle_4.emitting = true
+		picked_particle = 1
+		
 	
 	add_child(new_arrow)
